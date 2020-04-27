33832cd0f67dc6b90502dd6b7e88423cc8d16f55
Ben Veghte <BenVeghte@live.com>
2020-04-27 11:30:06 -0700

    Revert "Add listClassNames and listProfessors"
    This reverts commit 569546a3b3bbb3abd88b767b48a344510d83b607.

--- userclass.py
+++ userclass.py
@@ -4,15 +4,3 @@ class User:
         self.nickname = nickname
         self.classes = classes
 
-    #Function for Listing All of the classes they are currently taking
-    def listClassNames(self):
-        classNames =[]
-        for classdata in self.classes:
-            classNames.append(classdata["Class Name"])
-        return classNames
-    #Function for listing all of the professors they have
-    def listProfessors(self):
-        professors = []
-        for classdata in self.classes:
-            professors.append(classdata["Professor"])
-        return professors
