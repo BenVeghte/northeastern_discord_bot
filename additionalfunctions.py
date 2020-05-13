2d788399dbccaaed7af7ede3348d3decabd709fb
Ben Veghte <BenVeghte@live.com>
2020-05-13 11:11:14 -0700

    Add UIDtoAlpha function
    This function turns the numerical UID to letters

--- additionalfunctions.py
+++ additionalfunctions.py
@@ -6,3 +6,11 @@ def removeCommand(instring):
     print(instring)
     outstring = " ".join(instring[1:])
     return outstring
+
+def UIDtoAlpha(idnum):
+    idnum = str(idnum)
+    out = list()
+    alphabet = "abcdefghij"
+    for num in idnum:
+        out.append(alphabet[int(num)])
+    return ''.join(out)
