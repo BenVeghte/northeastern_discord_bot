e21fcf39e69e4fbdf08e3adc7eaddc7a0d5ce015
Ben Veghte <BenVeghte@live.com>
2020-05-12 20:00:52 -0700

    Fix Cource Class use of break

--- courseclass.py
+++ courseclass.py
@@ -13,18 +13,14 @@ class Course:
     
     #Adds a new professor to the list of professors if it isn't already there
     def addProf (self, new):
-        if new in self.professors:
-            break
-        else: 
+        if new not in self.professors:
             self.professors.append(new)
+
         return self.professors
 
     def addSection (self, new):
-        if new in self.sections:
-            break
-        else
+        if new  not in self.sections:
             self.sections.append(new)
-            
         return self.sections
     
 
